6822c8592391e355ca457e7264284327c053f323
ahmed nasser <ahmednas197@gmail.com>
2022-03-29 18:38:00 +0200

    add initial styling for app components

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import useStyles from './styles'
 
 export default function Form() {
+    const classes = useStyles();
+
     return (
         <div>
             <h1>From</h1></div>
