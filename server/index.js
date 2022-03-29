cd0190d9e11e5f01b3b62d5660f93590ece9bfd7
ahmed nasser <ahmednas197@gmail.com>
2022-03-29 15:41:55 +0200

    create the schema of the post message

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoutes from './routes/posts.js'
 
 const app = express();
 
@@ -10,10 +10,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-// app.use('/posts', postRoutes);
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Ahmed:Nasser2020@cluster0.lgxve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 5001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
