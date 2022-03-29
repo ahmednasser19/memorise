bb000162a7e28e464aa86f87ccd696c610b0c3d7
ahmed nasser <ahmednas197@gmail.com>
2022-03-29 18:16:54 +0200

    create app.js and Posts , Post , Form components for the frontend

---
+++ client/src/Components/Form/Form.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Form() {
+    return (
+        <div>
+            <h1>From</h1></div>
+    )
+}
---
+++ client/src/Components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Post from './Post/Post';
+
+export default function Posts() {
+    return (
+        <>
+            <h1> <Post /></h1>
+            <h1> <Post /></h1>
+        </>
+    )
+}
