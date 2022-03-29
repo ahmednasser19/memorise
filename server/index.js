0e33afcc774ab764ce63c5955e2c2cd13247d1ed
ahmed nasser <ahmednas197@gmail.com>
2022-03-29 15:22:21 +0200

    connect the backend to the databse

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 // app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Ahmed:Nasser2020@cluster0.lgxve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
