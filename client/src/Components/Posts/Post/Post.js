0b28c6cb02f752484e02b7ab868c4c8187ff0ecb
ahmed nasser <ahmednas197@gmail.com>
2022-04-11 15:39:08 +0200

    add like posts functionality

--- client/src/Components/Posts/Post/Post.js
+++ client/src/Components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 import { useDispatch } from 'react-redux';
 export default function Post({ post, setCurrentId }) {
     const classes = useStyles();
@@ -34,7 +34,7 @@ export default function Post({ post, setCurrentId }) {
                 <Typography className={classes.message} variant='h5' gutterBottom >{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => { }}>
+                <Button size="small" color="primary" onClick={() => { dispatch(likePost(post._id)) }}>
                     <ThumbUpAltIcon fontSize='small' />
                     Like
                     {post.likeCount}
