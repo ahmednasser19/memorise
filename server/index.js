17f24cabb8a48ba3f06cb71f449ef72f6c0f2210
ahmed nasser <ahmednas197@gmail.com>
2022-04-13 00:30:31 +0200

    make the app responsive for mobil devices

--- server/index.js
+++ server/index.js
@@ -16,8 +16,12 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://Ahmed:Nasser2020@cluster0.3mvgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) console.log(err)
+        else console.log("mongdb is connected");
+    });
+
+app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
 
 mongoose.set('useFindAndModify', false);
