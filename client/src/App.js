88b6fb5af82bbcd11fa34a351868a2f5ba6c4a30
ahmed nasser <ahmednas197@gmail.com>
2022-04-13 01:44:05 +0200

    add auth styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Container } from "@material-ui/core"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
+import Auth from './Components/Auth/Auth'
 export default function App() {
 
     return (
