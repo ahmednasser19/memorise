d7ae9df8950d7cc9a64174ffcd3484080bc07659
ahmed nasser <ahmednas197@gmail.com>
2022-04-13 23:34:01 +0200

    fix replicate emails

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,6 +33,7 @@ export const signup = async (req, res) => {
     try {
 
         const existingUser = await User.findOne(email);
+        if (existingUser) return res.status(404).json({ message: "User is already exists" });
 
         if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" });
 
