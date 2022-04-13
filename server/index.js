73a5a6148942407de7cc925090e6330f717183e9
ahmed nasser <ahmednas197@gmail.com>
2022-04-13 23:33:14 +0200

    add user routes and model in the backend

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.js';
 dotenv.config();
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 const PORT = process.env.PORT || 5001;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true },
