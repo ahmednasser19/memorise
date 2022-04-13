c339062d800557adfaf3c76bac73aea8d742a77d
ahmed nasser <ahmednas197@gmail.com>
2022-04-13 02:08:47 +0200

    add Input component
    it's an component to avoid repeating the text filed of sign up form

--- client/src/Components/Auth/Auth.js
+++ client/src/Components/Auth/Auth.js
@@ -1,7 +1,49 @@
-import React from 'react'
-
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import useStyles from './styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
 export default function Auth() {
+    const classes = useStyles();
+    const isSignup = false;
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    }
     return (
-        <div>Auth</div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant='h5'>
+                    {isSignup ? (
+                        'Sign Up'
+                    ) : (
+                        'Sign In'
+                    )}</Typography>
+                <from className={classes.form} onSubmit={handleSubmit}></from>
+                <Grid container spacing={2}>
+                    {isSignup && (
+                        <>
+                            <Grid xs={6} ms={12}>
+                                <TextField name='firstName' label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                            </Grid>
+
+                            <Grid xs={6} ms={12}>
+                                <TextField name='firstName' label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                            </Grid>
+
+                            <TextField name='lastName' label="Last Name" handleChange={handleChange} autoFocus xs={6} />
+
+                        </>
+                    )}
+                </Grid>
+            </Paper>
+
+        </Container>
     )
 }
